2a55f0bc72dd5fa5c7f5bbcbf1c669e468359782
Wintozzzzzzz <155535721+Wintozzzzzzz@users.noreply.github.com>
2024-04-10 19:08:09 +0400

    Add some new components and also footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ShopCategory from './Pages/ShopCategory';
 import ProductPage from './Pages/ProductPage';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
       
     </div>
