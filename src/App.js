ecf05444715fdc5c9be013236f06b4e0bd39b100
Wintozzzzzzz <155535721+Wintozzzzzzz@users.noreply.github.com>
2024-04-17 01:02:28 +0400

    Added Login Page with styles

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens"/>}/>
-        <Route path='/womens' element={<ShopCategory banner={women_banner} category="womens"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
         <Route path='product' element={<ProductPage/>}/>
         <Route path=':productId' element={<ProductPage/>}/>
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -5,10 +5,10 @@ import dropdown_icon from "../Components/Assets/dropdown_icon.png";
 import Item from "../Components/Item/Item";
 
 const ShopCategory = (props) => {
-  const { all_product } = useContext(ShopContext);
+  const { all_product } = useContext (ShopContext);
   return (
     <div className="shop-category">
-      <img src={props.banner} alt="banners" />
+      <img className="shopcategory-banner" src={props.banner} alt="banners" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
@@ -34,6 +34,9 @@ const ShopCategory = (props) => {
             return null;
           }
         })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
       </div>
     </div>
   );
