1327a0b7786d0ea13778fb9b9dbf630a8a7f9a94
Wintozzzzzzz <155535721+Wintozzzzzzz@users.noreply.github.com>
2024-04-24 22:18:14 +0400

    Update project

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (50)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit repellat perferendis blanditiis reiciendis adipisci expedita est libero pariatur quibusdam deleniti perspiciatis fugiat odio, dolore ducimus laudantium, consequatur aspernatur nihil impedit.
+        Nisi error assumenda perspiciatis fugit harum explicabo placeat excepturi, ducimus voluptates ipsum dignissimos labore? Quibusdam magnam in aliquid at, modi enim vero adipisci minima consequatur ea nam placeat, illum fugiat!
+        Totam reiciendis voluptatem architecto ab, facilis deserunt at. Iste, cumque illo amet aliquam a expedita laborum aperiam. Nobis tenetur maiores numquam deserunt, alias porro perferendis cum inventore sunt dolorum voluptatibus!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem aliquid rem animi, numquam repudiandae harum recusandae sed eos facere soluta incidunt eum. Unde officia temporibus provident doloribus vel esse ea?
+        Amet exercitationem velit at cum impedit nulla unde repellendus hic eaque in facilis quod fugiat atque expedita voluptatibus, quae culpa vel corporis qui quaerat officia optio doloremque facere. Unde, obcaecati!
+        Voluptate earum corporis, quibusdam at sit voluptatem incidunt odio. Nam voluptatem eos unde maiores debitis dolore quo voluptatum iusto porro corrupti, molestiae laudantium, quia tempora? Dolor sequi natus neque quaerat?
+        Incidunt quisquam doloribus, impedit, repudiandae adipisci voluptatum totam sunt laboriosam rem aut non earum facilis illo ut autem provident eligendi sit ea architecto placeat inventore quas facere. Repellendus, explicabo saepe.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
