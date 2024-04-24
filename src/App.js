facb9e83fd74b86a123dc4ab482611eb0181059e
Wintozzzzzzz <155535721+Wintozzzzzzz@users.noreply.github.com>
2024-04-24 10:24:51 +0400

    some update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar/Navbar';
 import{BrowserRouter, Routes, Route} from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
-import ProductPage from './Pages/ProductPage';
+import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
@@ -22,8 +22,8 @@ function App() {
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
-        <Route path='product' element={<ProductPage/>}/>
-        <Route path=':productId' element={<ProductPage/>}/>
+        <Route path='product' element={<Product/>}/>
+        <Route path=':productId' element={<Product/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
